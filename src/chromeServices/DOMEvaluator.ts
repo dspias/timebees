3085114bcbd20d8baaeabba4aee0397398981064
Pias Das <pdsperrow95@gmail.com>
2023-04-27 13:01:08 +0600

    Check storage when text selected

--- src/chromeServices/DOMEvaluator.ts
+++ src/chromeServices/DOMEvaluator.ts
@@ -2,46 +2,46 @@ import './TimeComponent';
 import DetectTime from './DetectTime';
 import ConvertTime from './ConvertTime';
 
-chrome.storage.sync.get().then((storage) => {
-  const timeComponent = document.createElement("time-component");
-  document.body.appendChild(timeComponent);
+const timeComponent = document.createElement("time-component");
+document.body.appendChild(timeComponent);
 
-  // set time component position
-  const setContext = (context:any) =>
-  timeComponent.setAttribute(
-    "context",
-    JSON.stringify(context)
-  );
+// set time component position
+const setContext = (context:any) =>
+timeComponent.setAttribute(
+  "context",
+  JSON.stringify(context)
+);
 
-  // get time component position 
-  const getTimeComponentPosition = (time: string, abbreviation?: string | null, convertedTime?: string | null) => {
-    const rangeBounds = window
-      ?.getSelection()
-      ?.getRangeAt(0)
-      ?.getBoundingClientRect();
-      console.log(rangeBounds);
-    const timeAnchor = rangeBounds && {
-      left: rangeBounds.left,
-      top: rangeBounds.top + window.pageYOffset ,
-      width: rangeBounds.width,
-      height: rangeBounds.height
-    };
-    const timeBubble = rangeBounds && {
-      left: rangeBounds.left + rangeBounds.width / 2 - 150,
-      top: rangeBounds.top + window.pageYOffset + rangeBounds.height + 10,
-      visibility: 'visible'
-    };
-    return {
-      anchor: timeAnchor,
-      bubble: timeBubble,
-      localtimezone: storage?.localtimezone,
-      time: { time, abbreviation, convertedTime }
-    };
+// get time component position 
+const getTimeComponentPosition = (time: string, abbreviation?: string | null, convertedTime?: string | null, localtimezone?: string | null) => {
+  const rangeBounds = window
+    ?.getSelection()
+    ?.getRangeAt(0)
+    ?.getBoundingClientRect();
+    console.log(rangeBounds);
+  const timeAnchor = rangeBounds && {
+    left: rangeBounds.left,
+    top: rangeBounds.top + window.pageYOffset ,
+    width: rangeBounds.width,
+    height: rangeBounds.height
   };
-  
-  const getSelectedText = () => window.getSelection()?.toString();
-  
-  document.addEventListener("click", () => {
+  const timeBubble = rangeBounds && {
+    left: rangeBounds.left + rangeBounds.width / 2 - 150,
+    top: rangeBounds.top + window.pageYOffset + rangeBounds.height + 10,
+    visibility: 'visible'
+  };
+  return {
+    anchor: timeAnchor,
+    bubble: timeBubble,
+    localtimezone: localtimezone,
+    time: { time, abbreviation, convertedTime }
+  };
+};
+
+const getSelectedText = () => window.getSelection()?.toString();
+
+document.addEventListener("click", () => {
+  chrome.storage.sync.get().then((storage) => {
     const selectedText = getSelectedText();    
     if (selectedText && selectedText.length > 0) {
       const isTrack = storage?.isalltrack;
@@ -49,14 +49,16 @@ chrome.storage.sync.get().then((storage) => {
         const [time, abbreviation] = DetectTime(selectedText);
         if (time && abbreviation) {
           const convertedTime = ConvertTime(time, abbreviation, storage?.localtimezone);
-          const position= getTimeComponentPosition(time, abbreviation, convertedTime);
+          const position= getTimeComponentPosition(time, abbreviation, convertedTime, storage?.localtimezone);
           setContext(position);
         }
       } 
     }
   });
-  
-  document.addEventListener("selectionchange", () => {
+});
+
+document.addEventListener("selectionchange", () => {
+  chrome.storage.sync.get().then((storage) => {
     const selectedText = getSelectedText();
     if (selectedText?.length === 0 || !selectedText) {
       setContext({bubble: { visibility: 'hidden' }, localtimezone: storage?.localtimezone, time: null });
